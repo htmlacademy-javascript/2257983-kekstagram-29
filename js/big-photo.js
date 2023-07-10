f8eab6d4080e4a61d79f87af07857adc6f3e385d
Егор Мирошниченко <beowulf1502mail@gmail.com>
2023-07-10 14:17:42 +0200

    Создает 5 комментариев

--- js/big-photo.js
+++ js/big-photo.js
@@ -1,5 +1,8 @@
 import {pressEscButton} from './utils.js';
 
+const COMMENT_PER_CLICK = 5;
+let defaultCommentCount = 0;
+
 //Попап
 const bigPicture = document.querySelector('.big-picture');
 const escButton = document.querySelector('.big-picture__cancel');
@@ -7,12 +10,13 @@ const escButton = document.querySelector('.big-picture__cancel');
 //Содержимое попапа
 const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
 const bigPictureLikes = bigPicture.querySelector('.likes-count');
-const bigPictureComments = bigPicture.querySelector('.social__comment-count');
+const bigPictureComments = bigPicture.querySelector('.comments-count');
 const bigPictureDescription = bigPicture.querySelector('.social__caption');
 
 //Содержимое комментариев
 const defaultComments = document.querySelector('.social__comments');
 const commentElement = document.querySelector('.social__comment');
+const loadCommentsButton = document.querySelector('.comments-loader');
 
 escButton.addEventListener('click', () => { //Закрываем фото по клику
   bigPicture.classList.add('hidden');
@@ -32,26 +36,30 @@ const renderBigPhoto = (data) => { //Создаем большое фото бе
   bigPictureDescription.textContent = data.description;
 };
 
-const renderOneComment = (element) => { //Создаем 1 комментарий
+const renderOneComment = (data) => { //Создаем 1 комментарий
   const comment = commentElement.cloneNode(true);
 
-  comment.querySelector('.social__picture').src = element.avatar;
-  comment.querySelector('.social__picture').alt = element.name;
-  comment.querySelector('.social__text').textContent = element.message;
-
-  return comment;
+  comment.querySelector('.social__picture').src = data.avatar;
+  comment.querySelector('.social__picture').alt = data.name;
+  comment.querySelector('.social__text').textContent = data.message;
 };
 
-const fillCommentsList = (data) => { //Перебираем массив для большего числа комментариев
-  data.forEach((element) => {
-    defaultComments.append(renderOneComment(element));
-  });
+const fillCommentsList = (data) => { //Создаем по 5 комментариев
+  defaultCommentCount += COMMENT_PER_CLICK;
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < defaultCommentCount; i++) {
+    const comment = renderOneComment(data[i]);
+    fragment.append(comment);
+  }
+
+  defaultComments.append(fragment);
 };
 
 const showBigPictures = (data) => { //Создаем функцию для открытия картинки с комментариями
-  defaultComments.innerHTML = '';
-  renderBigPhoto(data);
-  fillCommentsList(data.comments);
+  defaultComments.innerHTML = ''; //Очищаем список комментариев
+  renderBigPhoto(data); //Создаем большие фотографии
+  fillCommentsList(data.comments); //Показываем комментарии
 };
 
 export {showBigPictures};
